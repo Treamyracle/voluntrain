05fbb2e13eb5a416c33f11358a5df5425cc61e14
Arif Athaya Harahap <arifathayaharahap@gmail.com>
2025-12-16 09:33:01 +0700

    First commit: Initial release of VolunTrain library

---
+++ src/voluntrain/host.py
@@ -0,0 +1,4 @@
+# Change: import zmq 
+# To:
+import zmq
+from .protocol import encode_id, serialize, deserialize # Note the dot!
---
+++ src/voluntrain/worker.py
@@ -0,0 +1,2 @@
+import zmq
+from .protocol import decode_id, serialize, deserialize # Note the dot!
